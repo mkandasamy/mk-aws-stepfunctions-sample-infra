663164b5b0aef47e2433d6e90dd52c55ff92cf8a
mkandasamy <mohankandasami@gmail.com>
2019-04-24 03:42:53 -0400

    atlantis  test stepfunctions

--- env/latest.tfvars
+++ env/latest.tfvars
@@ -1,6 +1,10 @@
-app_name		 = "mk-aws-stepfunctions-sample"
-environment		 = "latest"
-lambda_role_name = "mk-aws-stepfunctions-sample-lambda-role"
+app_name		 		= "mk-aws-stepfunctions-sample"
+environment		 		= "latest"
+region					= "us-east-2"
+account_id				= "203532026745"
+lambda_role_name 		= "mk-aws-stepfunctions-sample-lambda-role"
+stepfunction_name		= "mk-aws-stepfunctions-sample"
+stepfunction_role_name  = "mk-aws-stepfunctions-sample-role"
 
 
 lambda_variables = [
--- sample.tf
+++ sample.tf
@@ -13,6 +13,17 @@ resource "aws_iam_role_policy" "lambda_policy" {
   policy = "${file("${path.module}/policies/lambda-role-policy.json")}"
 }
 
+resource "aws_iam_role" "stepfunction_role" {
+  name = "${var.stepfunction_role_name}"
+  assume_role_policy = "${file("${path.module}/policies/stepfunction-role.json")}"
+}
+
+resource "aws_iam_role_policy" "stepfunction_policy" {
+  name = "${var.stepfunction_role_name}-policy"
+  role = "${aws_iam_role.stepfunction_role.id}"
+  policy = "${file("${path.module}/policies/stepfunction-role-policy.json")}"
+}
+
 resource "aws_lambda_function" "default" {
   count                          = "${length(var.lambda_variables)}"
   function_name                  = "${lookup(var.lambda_variables[count.index],"name")}-${var.environment}"
@@ -25,4 +36,21 @@ resource "aws_lambda_function" "default" {
   publish                        = "${lookup(var.lambda_variables[count.index],"lambda_publish")}"
   reserved_concurrent_executions = "${lookup(var.lambda_variables[count.index],"reserved_concurrent_executions")}"
   filename         				 = "${path.module}/artifacts/${lookup(var.lambda_variables[count.index],"name")}.zip"
+}
+
+data "template_file" "sfn_state_machine_data" {
+  template = "${file("${path.module}/artifacts/mk-aws-stepfunctions-sample.sf")}"
+
+  vars {
+    region                    = "${var.region}"
+    account_id                = "${var.account_id}"
+    lambda_name               = "${lookup(var.lambda_variables[count.index],"name")}-${var.environment}"
+  }
+}
+
+resource "aws_sfn_state_machine" "sfn_state_machine" {
+  name     = "${var.stepfunction_name}"
+  role_arn = "${aws_iam_role.stepfunction_role.arn}"
+
+  definition = "${data.template_file.sfn_state_machine_data.rendered}"
 }
